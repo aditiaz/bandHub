3f3668ed352757732e471948527767dd656faa6a
aditiaz <aditiaan96a@gmail.com>
2025-02-01 16:07:37 +0700

    smol changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,26 +9,33 @@
     width: 100vw;
     height: 100vh;
     background: url('../assets/1.svg');
-    overflow: hidden;
+   padding-top: 20%;
 }
   .judul {
     color: white;
     font-family: 'Arial', sans-serif; /* Ganti dengan font yang sesuai */
-    font-size: 24px; /* Sesuaikan ukuran font */
+    font-size: 3.5rem; /* Sesuaikan ukuran font */
     font-weight: bold; /* Sesuaikan ketebalan font */
-    letter-spacing: 1.5px; /* Sesuaikan jarak antar huruf */
-    text-transform: uppercase; /* Jika teks di SVG adalah huruf kapital */
+    letter-spacing: 1.5px; 
     text-align: center; /* Pusatkan teks */
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%); /* Pusatkan teks di tengah */
 }
+.sub-judul{
+    color: white;
+    font-family: 'Arial', sans-serif; /* Ganti dengan font yang sesuai */
+    font-size: 1rem; /* Sesuaikan ukuran font */
+    letter-spacing: .2rem;
+    font-weight: lighter;
+    text-align: center; /* Pusatkan teks */
+}
+
+
 
 
 </style>
 
 <div class="background">
-    <!-- <img src={background} alt="Background" /> -->
-     <p class="judul">Menghubungkan Band dengan IO</p>
+  <div >
+    <p class="judul">Band Hub</p>
+    <p class="sub-judul">Menghubungkan band dengan EO</p>
+  </div>
 </div>
